cdba011e0f0a7e82f1a2c273950ed0e10b7e7975
Thulara <thularablockchain@gmail.com>
2024-01-24 03:30:41 +1100

    create Links for navbar components

--- frontend-for-server/src/components/NavBar.jsx
+++ frontend-for-server/src/components/NavBar.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min"
+
 export default function NavBar(){
 
     return (
-        <h1>NavBar</h1>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/showdata"> Card</Link>
+        </nav>
     )
 
 }
