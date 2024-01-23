3af33873561ef23a30cff097a329bbe93ceb730f
Thulara <thularablockchain@gmail.com>
2024-01-24 01:27:23 +1100

    Correct the page name

--- frontend-for-server/src/App.js
+++ frontend-for-server/src/App.js
@@ -1,31 +1,40 @@
-import './App.css';
-import { useEffect, useState } from 'react'
-import CardDb from './components/CardDb';
-import { BrowserRouter, Route } from 'react-router-dom';
+import "./App.css";
+import { useEffect, useState } from "react";
+import CardDb from "./components/CardDb";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
-  const [dbList, setDbList] = useState([])
+  const [dbList, setDbList] = useState([]);
 
-  const showCard = dbList.map((card) => <CardDb name={card.name} image={card.image}/>)
+  const showCard = dbList.map((card) => (
+    <CardDb key ={card.id} name={card.name} image={card.image} />
+  ));
 
   async function fetchDb() {
-    const response = await fetch('https://json-server-poke.onrender.com/card')
-    const data = await response.json()
+    const response = await fetch("https://json-server-poke.onrender.com/card");
+    const data = await response.json();
 
-    setDbList(data)
-    console.log(data)
+    setDbList(data);
+    console.log(data);
   }
-  useEffect(()=>{
-    fetchDb()
-  })
+  useEffect(() => {
+    fetchDb();
+  }, []);
+
   return (
-    <BrowserRouter>    
-      <Route path="/">
+    <BrowserRouter>
+      <Route exact path="/">
         <div className="App">
-        <div className="card-container">
-          {showCard}
+          <div className="card-container">{showCard}</div>
         </div>
-        </div>
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/contact">
+        <Contact />
       </Route>
     </BrowserRouter>
   );
