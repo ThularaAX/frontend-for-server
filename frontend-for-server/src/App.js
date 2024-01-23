6b85f42a0543c94941544a579e3ddfe987e897da
Thulara <thularablockchain@gmail.com>
2024-01-23 18:41:07 +1100

    fetch data from deployed backend using async function

--- frontend-for-server/src/App.js
+++ frontend-for-server/src/App.js
@@ -5,10 +5,16 @@ import CardDb from './components/CardDb';
 function App() {
   const [dbList, setDbList] = useState([])
 
-  const showCard = dbList.map((card)=> <CardDb name={card.name}/>)
+  const showCard = dbList.map((card) => <CardDb name={card.name} />)
 
+  async function fetchDb() {
+    const response = await fetch('https://json-server-poke.onrender.com/card')
+    const data = await response.json()
+    
+    console.log(data)
+  }
   useEffect(()=>{
-    console.log("side effects")
+    fetchDb()
   })
   return (
     <div className="App">
