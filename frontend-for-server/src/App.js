52e3d6cf10c41db94e1a282fc4273992411a40ec
Thulara <thularablockchain@gmail.com>
2024-01-23 17:57:15 +1100

    create map methode for showing each card name

--- frontend-for-server/src/App.js
+++ frontend-for-server/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-import { useEffect} from 'react'
+import { useEffect, useState } from 'react'
+import CardDb from './components/CardDb';
 
 function App() {
+  const [dbList, setDbList] = useState([])
+
+  const showCard = dbList.map((card)=> <CardDb name={card.name}/>)
+
   useEffect(()=>{
     console.log("side effects")
   })
   return (
     <div className="App">
-     
+     {showCard}
     </div>
   );
 }
