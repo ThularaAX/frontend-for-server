dfad4fa025a804fc639d37b502a75df0dcc30632
Thulara <thularablockchain@gmail.com>
2024-01-24 03:43:32 +1100

    Add common css to all the elements inside navbar

--- frontend-for-server/src/components/NavBar.jsx
+++ frontend-for-server/src/components/NavBar.jsx
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min"
 
 export default function NavBar(){
 
-    
+    const style =   
+        { color: 'white',
+        backgroundColor: "gray" }
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/showdata"> Card</Link>
+            <Link style={style} to="/">Home</Link>
+            <Link style={style} to="/about">About</Link>
+            <Link style={style} to="/contact">Contact</Link>
+            <Link style={style} to="/showdata"> Card</Link>
         </nav>
     )
 
