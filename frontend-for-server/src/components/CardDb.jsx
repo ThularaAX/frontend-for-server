b4313afc2bca7f06075c506cc81d19a53be57b93
Thulara <thularablockchain@gmail.com>
2024-01-23 22:59:50 +1100

    clean cardDb component

--- frontend-for-server/src/components/CardDb.jsx
+++ frontend-for-server/src/components/CardDb.jsx
@@ -3,12 +3,9 @@
 export default function CardDb(props){
     
     return(
-        // <div className=".card-container">
            <div name="card" className="card-list">
             <h2>{props.name}</h2>
             <img src={props.image} alt={props.name}/>
-            </div> 
-        // </div>
-        
+            </div>        
     )
 }
