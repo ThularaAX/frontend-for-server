9936d2b1f13957f3e9fc48c99436a126c957eecf
Thulara <thularablockchain@gmail.com>
2024-01-23 17:21:45 +1100

    Initial setup of the Card component

--- frontend-for-server/src/components/CardDb.jsx
+++ frontend-for-server/src/components/CardDb.jsx
@@ -1,3 +1,10 @@
+import { useState, useEffect } from "react";
+
 export default function CardDb(){
     
+    return(
+        <div name="card" className="card-list">
+            <h2>Cards</h2>
+        </div>
+    )
 }
